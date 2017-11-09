b0d2baa00a8d90da1f7ea8ae7b39b8ccd849a4a8
OgliariNatan <natanogliari@gmail.com>
2017-11-09 10:00:52 -0200

    Adicionado projeto da leiitura de arquivos && atualizado projeto de FP

---
+++ trampo_FP/trampo_FP.asm
@@ -0,0 +1,24 @@
+#######Author Natan Ogliari######
+#################################
+#No main: Solicitar 3 valeres de um vetor
+#No multi: Multipliar por um vetor com
+########################################
+
+.data
+pergunta: .asciiz "Entre com um valor \n"
+
+
+.text
+  main:
+        la $a0, pergunta
+        li $v0, 4 #Prepara para imprimir na tela
+        syscall
+
+        li $v0, 6 #Prepara para leitura
+        syscall
+        move   $t0, $v0    #  =  
+
+  mult:
+
+
+  fim:
